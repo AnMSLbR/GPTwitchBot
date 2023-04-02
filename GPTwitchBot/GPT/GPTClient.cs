59d4b4d6c058c030d85f79daf306e252799ca156
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2023-04-02 20:48:09 +0500

    Feature-1. Functionality for chatting with chatGPT implemented.

---
+++ GPTwitchBot/GPT/GPTClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GPTwitchBot.GPT
+{
+    internal class GPTClient
+    {
+        string _apiKey = "api_Key";
+        string _endpoint = "https://api.openai.com/v1/chat/completions";
+        List<Message> _messages;
+        HttpClient _httpClient;
+
+        public List<Message> Messages { get => _messages; set => _messages = value; }
+
+        public GPTClient()
+        {
+            _messages = new List<Message>();
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
+        }
+
+        public async Task<string> ChatAsync(Message message)
+        {
+            _messages.Add(message);
+            var requestData = new Request() { ModelId = "gpt-3.5-turbo", Messages = _messages };
+
+            using var response = await _httpClient.PostAsJsonAsync(_endpoint, requestData);
+            ResponseData? responseData = await response.Content.ReadFromJsonAsync<ResponseData>();
+            var choices = responseData?.Choices ?? new List<Choice>();
+            if (choices.Count == 0)
+            {
+                return "No choices were returned by the API";
+            }
+            var choice = choices[0];
+            var responseMessage = choice.Message;
+            _messages.Add(responseMessage);
+            return responseMessage.Content.Trim();      
+        }
+
+    }
+}
--- GPTwitchBot/Program.cs
+++ GPTwitchBot/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GPTwitchBot.GPT;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+
+GPTClient gPTClient = new GPTClient();
+
+while (true)
+{
+    Console.Write("User: ");
+    var content = Console.ReadLine();
+
+    if (content is not { Length: > 0 }) break;
+    var message = new Message() { Role = "user", Content = content };
+
+    var answer = await gPTClient.ChatAsync(message);
+    Console.WriteLine($"ChatGPT: {answer}");
+}
