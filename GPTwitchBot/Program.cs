36ac64c9de14c5640a3739a1ca83c9301633b3c8
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2023-04-04 18:24:24 +0500

    Feature-2. The functionality of interaction with Twitch chat implemented. ChatGPT replies to mention messages and question messages.

--- GPTwitchBot/Program.cs
+++ GPTwitchBot/Program.cs
@@ -1,18 +1,54 @@
 ﻿using GPTwitchBot.GPT;
-using System.Net.Http.Json;
-using System.Text.Json.Serialization;
+using GPTwitchBot.Twitch;
+
+GPTClient gptClient = new GPTClient();
+string answer = "";
+Message message;
+string preset = "Answer on behalf of a person, do not mention that you are a neural network or a computer program, write the answer shorter, answer informally and in the masculine form: ";
+
+TwitchBot bot = new TwitchBot("your_token", "botChannel", "streamChannel");
+bot.OnChatMentionMessageReceived += Bot_OnChatMentionMessageReceived;
+bot.OnChatQuestionMessageReceived += Bot_OnChatQuestionMessageReceived;
+
+bot.Start();
 
 
-GPTClient gPTClient = new GPTClient();
+async void Bot_OnChatMentionMessageReceived(object? sender, EventArgs e)
+{
+    if (bot.MessageSender.ToLower() != bot.BotChannel)
+    {
+        await ReplyToMessageAsync(bot.MessageSender, bot.ReceivedMessage);
+    }
+}
+
+async void Bot_OnChatQuestionMessageReceived(object? sender, EventArgs e)
+{
+    if (bot.MessageSender.ToLower() != bot.BotChannel && bot.ReceivedMessage.Contains('@') == false)
+    {
+        await ReplyToMessageAsync(bot.MessageSender, bot.ReceivedMessage);
+    }
+}
+
+async Task ReplyToMessageAsync(string sender, string text)
+{
+    Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {sender}: {text}");
+    var content = preset + text;
+    message = new Message() { Role = "user", Content = content };
+    answer = await gptClient.ChatAsync(message);
+    Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} ChatGPT: {answer}");
+    while (answer.Length > 250)
+    {
+        message = new Message() { Role = "user", Content = "make it short" };
+        answer = await gptClient.ChatAsync(message);
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} ChatGPT: {answer}");
+    }
+    bot.Send("@" + sender + " " + answer);
+}
 
 while (true)
 {
-    Console.Write("User: ");
     var content = Console.ReadLine();
-
-    if (content is not { Length: > 0 }) break;
-    var message = new Message() { Role = "user", Content = content };
-
-    var answer = await gPTClient.ChatAsync(message);
-    Console.WriteLine($"ChatGPT: {answer}");
+    if (content?.ToLower() == "stop") break;
 }
+
+bot.Stop();
---
+++ GPTwitchBot/Twitch/TwitchBot.cs
@@ -0,0 +1,77 @@
+﻿using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace GPTwitchBot.Twitch
+{
+    internal class TwitchBot
+    {
+        string _token = "";
+        string _botChannel = "";
+        string _streamChannel = "";
+        TwitchClient _client;
+        ConnectionCredentials _credentials;
+
+        public string BotChannel { get => _botChannel; set => _botChannel = value; }
+
+        private string _receivedMessage = null;
+        public string ReceivedMessage { get => _receivedMessage; }
+
+        private string messageSender = null;
+        public string MessageSender { get => messageSender; }
+
+        public event EventHandler OnChatMentionMessageReceived;
+        public event EventHandler OnChatQuestionMessageReceived;
+
+        public TwitchBot(string token, string bot, string streamer)
+        {
+            _token = token;
+            _botChannel = bot.ToLower();
+            _streamChannel = streamer.ToLower();
+            _credentials = new ConnectionCredentials(BotChannel, _token);
+        }
+
+        public void Start()
+        {
+            _client = new TwitchClient();
+            _client.Initialize(_credentials, _streamChannel);
+            _client.OnMessageReceived += Client_OnMentionMessageReceived;
+            _client.OnMessageReceived += Client_OnQuestionMessageReceived;
+            _client.Connect();
+        }
+
+        private void Client_OnQuestionMessageReceived(object? sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Message.ToLower().Contains('?') && (e.ChatMessage.Message.ToLower().Contains(BotChannel) == false))
+            {
+                _receivedMessage = e.ChatMessage.Message;
+                messageSender = e.ChatMessage.DisplayName;
+                OnChatQuestionMessageReceived.Invoke(this, new EventArgs());
+            }
+        }
+
+        private void Client_OnMentionMessageReceived(object? sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Message.ToLower().Contains(BotChannel))
+            {
+                _receivedMessage = e.ChatMessage.Message.Substring(BotChannel.Length + 1);
+                messageSender = e.ChatMessage.DisplayName;
+                OnChatMentionMessageReceived.Invoke(this, new EventArgs());
+            }
+        }
+
+        public void Send(string message)
+        {
+            _client.SendMessage(_client.GetJoinedChannel(_streamChannel), message);
+        }
+
+        public void Stop()
+        {
+            if (_client != null)
+            {
+                _client.Disconnect();
+                _client = null;
+            }
+        }
+    }
+}
